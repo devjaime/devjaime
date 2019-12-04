e5c8fc5e94d3800081e46e2a6e5d41495a166f3b
Jaime Hernandez <hernandez.hs@gmail.com>
2019-12-04 01:46:05 -0300

    cambios realizados para ocultar mapa

--- js/main.js
+++ js/main.js
@@ -172,69 +172,69 @@
     });
 
     // Intialize Map
-    google.maps.event.addDomListener(window, 'load', init);
+    /* google.maps.event.addDomListener(window, 'load', init);
 
-    function init() {
-        // Basic options for a simple Google Map
-        // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-        var mapOptions = {
-            // How zoomed in you want the map to start at (always required)
-            zoom: 11,
+     function init() {
+         // Basic options for a simple Google Map
+         // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+         var mapOptions = {
+             // How zoomed in you want the map to start at (always required)
+             zoom: 11,
 
-            // The latitude and longitude to center the map (always required)
-            center: new google.maps.LatLng(-33.4377968, -70.6504451),
+             // The latitude and longitude to center the map (always required)
+             center: new google.maps.LatLng(-33.4377968, -70.6504451),
 
-            scrollwheel: false,
+             scrollwheel: false,
 
 
-            // How you would like to style the map.
-            // This is where you would paste any style found on Snazzy Maps.
-            styles: [{
-                featureType: 'all',
-                stylers: [{
-                    saturation: -65
-                }]
-            }, {
-                featureType: 'road.arterial',
-                elementType: 'geometry',
-                stylers: [{
-                    hue: '#00ffee'
-                }, {
-                    saturation: 80
-                }]
-            }, {
-                featureType: 'poi.business',
-                elementType: 'labels',
-                stylers: [{
-                    visibility: 'off'
-                }]
-            }]
-        };
+             // How you would like to style the map.
+             // This is where you would paste any style found on Snazzy Maps.
+             styles: [{
+                 featureType: 'all',
+                 stylers: [{
+                     saturation: -65
+                 }]
+             }, {
+                 featureType: 'road.arterial',
+                 elementType: 'geometry',
+                 stylers: [{
+                     hue: '#00ffee'
+                 }, {
+                     saturation: 80
+                 }]
+             }, {
+                 featureType: 'poi.business',
+                 elementType: 'labels',
+                 stylers: [{
+                     visibility: 'off'
+                 }]
+             }]
+         };
 
-        // Get the HTML DOM element that will contain your map
-        // We are using a div with id="map" seen below in the <body>
-        var mapElement = document.getElementById('map');
+         // Get the HTML DOM element that will contain your map
+         // We are using a div with id="map" seen below in the <body>
+         var mapElement = document.getElementById('map');
 
-        // Create the Google Map using our element and options defined above
-        var map = new google.maps.Map(mapElement, mapOptions);
+         // Create the Google Map using our element and options defined above
+         var map = new google.maps.Map(mapElement, mapOptions);
 
-        var image = 'images/map-marker.png';
-        // Let's also add a marker while we're at it
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(-33.4377968, -70.6504451),
-            map: map,
-            icon: image,
-            draggable: true,
-            animation: google.maps.Animation.DROP
-        });
-        marker.addListener('click', toggleBounce);
+         var image = 'images/map-marker.png';
+         // Let's also add a marker while we're at it
+         var marker = new google.maps.Marker({
+             position: new google.maps.LatLng(-33.4377968, -70.6504451),
+             map: map,
+             icon: image,
+             draggable: true,
+             animation: google.maps.Animation.DROP
+         });
+         marker.addListener('click', toggleBounce);
 
-        function toggleBounce() {
-            if (marker.getAnimation() !== null) {
-                marker.setAnimation(null);
-            } else {
-                marker.setAnimation(google.maps.Animation.BOUNCE);
-            }
-        }
-    }
+         function toggleBounce() {
+             if (marker.getAnimation() !== null) {
+                 marker.setAnimation(null);
+             } else {
+                 marker.setAnimation(google.maps.Animation.BOUNCE);
+             }
+         }
+     }*/
 })(jQuery);
